ccd0ec608c6a680a7e3492194be02a6dd75627a1
GiuntaGiovanni <jhonniest741@gmail.com>
2025-04-12 15:07:49 +0200

    Refactor Header component for improved mobile and desktop navigation; update styles for better layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,40 +7,45 @@ export default function Header() {
         <>
             <header className={style.header}>
                 {/* external box */}
-                <div className="container">
-
-                    <nav className="navbar d-md-none">
-                        {/* burgher button */}
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                {navLink.map(link => {
+                <div className={`container`}>
+                    <div className={style.nav_container}>
+                        {/* navBar-tablet & pc*/}
+                        <nav className="d-none d-md-block">
+                            <ul className="row p-0 m-0">
+                                {navLink.map((link, index) => {
                                     return (
-                                        <li className="nav-item">
-                                            <a className="nav-link active" aria-current="page" href="#">{link}</a>
+                                        <li className="col" key={index}>
+                                            <a href="">{link}</a>
                                         </li>
                                     )
                                 })}
+
                             </ul>
+                        </nav>
+
+                        {/* navbar-mobile */}
+                        <div className="row w-100 m-0">
+                            <div className="col-12 p-0">
+                                <nav className="navbar d-md-none">
+                                    {/* burgher button */}
+                                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                                        <span className="navbar-toggler-icon"></span>
+                                    </button>
+                                    <div className="collapse navbar-collapse w-100 mt-2" id="navbarTogglerDemo02">
+                                        <ul className="navbar-nav w-100 text-center mb-2 mb-lg-0 p-0 m-0">
+                                            {navLink.map((link, index) => {
+                                                return (
+                                                    <li className="nav-item w-100" key={index}>
+                                                        <a className="nav-link active" aria-current="page" href="#">{link}</a>
+                                                    </li>
+                                                )
+                                            })}
+                                        </ul>
+                                    </div>
+                                </nav>
+                            </div>
                         </div>
-                    </nav>
-
-                    {/* navBar */}
-                    <nav className="d-none d-md-block">
-                        <ul className="row p-0">
-                            {navLink.map(link => {
-                                return (
-                                    <li className="col">
-                                        <a href="">{link}</a>
-                                    </li>
-                                )
-                            })}
-
-                        </ul>
-                    </nav>
-
+                    </div>
                 </div >
             </header >
         </>
