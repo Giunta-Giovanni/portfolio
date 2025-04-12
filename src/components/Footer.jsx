82116ab69fb3cc106833a00fb21c7a8e3f1de0c3
GiuntaGiovanni <jhonniest741@gmail.com>
2025-04-12 15:56:24 +0200

    Add FontAwesome icons and update Footer component styling for social media links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,47 @@
+// import fontAwesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faLinkedin, faTwitter, faInstagram } from "@fortawesome/free-brands-svg-icons";
+// import style
+import style from "./Footer.module.css";
+
 export default function Footer() {
     return (
         <>
-            <div>questo è il footer</div>
+            <footer className={style.footer}>
+                {/* external container */}
+                <div className="container">
+                    <div className={style.channel_dimension_box}>
+                        {/* channel */}
+                        <div className={`row row-cols-4 ${style.channel_box}`}>
+                            {/* Linkedin */}
+                            <div className={`col ${style.single_channel}`}>
+                                <a href="">
+                                    <FontAwesomeIcon icon={faLinkedin} />
+                                </a>
+                            </div>
+                            {/* twitter */}
+                            <div className={`col ${style.single_channel}`}>
+                                <a href="">
+                                    <FontAwesomeIcon icon={faTwitter} />
+                                </a>
+                            </div>
+                            {/* facebook */}
+                            <div className={`col ${style.single_channel}`}>
+                                <a href="">
+                                    <FontAwesomeIcon icon={faFacebook} />
+                                </a>
+                            </div>
+                            {/* instagram */}
+                            <div className={`col ${style.single_channel}`}>
+                                <a href="">
+                                    <FontAwesomeIcon icon={faInstagram} />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </footer>
         </>
     )
 }
