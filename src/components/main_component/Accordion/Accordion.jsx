ab6661000fa09debeea9e38a31c204eba940fdcd
GiuntaGiovanni <jhonniest741@gmail.com>
2025-04-14 23:15:46 +0200

    [feat] made accordion dynamic

--- src/components/main_component/Accordion/Accordion.jsx
+++ src/components/main_component/Accordion/Accordion.jsx
@@ -1,21 +1,27 @@
 import style from "./accordion.module.css";
-export default function AccordionItem({ isActive, onClick }) {
+export default function AccordionItem({ isActive, onClick, }) {
     return (
 
         // Accordion Item
         <div className={`${style.item} ${isActive ? style.active : ''}`} onClick={onClick}>
             {/* icon */}
-            <div className={`${style.icon_container}  ${isActive ? style.active : ''}`}>
-                <div className={`${style.stroke_one}  ${isActive ? style.active : ''}`}></div>
-                <div className={`${style.stroke_two}`}></div>
-            </div>
+
 
             {/* accordion title */}
-            <div className="row">
-                <div className="col-7">
-                    <h1 className={style.title}>Introduction to React JS</h1>
+            <div className="row align-items-center">
+                <div className="col-1 p-0">
+                    <div className={style.test}></div>
+                    <div className={` ${style.icon_container}  ${isActive ? style.active : ''}`}>
+                        <div className={`${style.stroke_one}  ${isActive ? style.active : ''}`}></div>
+                        <div className={`${style.stroke_two}`}></div>
+                    </div>
+
                 </div>
-                <div className="col-5 d-flex justify-content-end">
+                <div className="col-8 p-0 ">
+                    <h1 className={style.tile}>Introduction to React JS</h1>
+                </div>
+
+                <div className="col-3 d-flex justify-content-end align-items-center">
                     <div className={style.project_link_box}>
                     </div>
                     <div className={style.project_link_box}>
@@ -23,9 +29,16 @@ export default function AccordionItem({ isActive, onClick }) {
                 </div>
 
                 {/* accordion content */}
-                <p className={`${style.content}  ${isActive ? style.active : ''}`}>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veritatis aperiam ipsam repellendus eos sint incidunt voluptatibus sequi nisi, a laboriosam, sit ratione officia consequatur corporis tenetur corrupti delectus aliquid.
-                </p>
+
+                <div className={`${style.content}  ${isActive ? style.active : ''}`}>
+                    <h2>
+                        Titolo intestazione
+                    </h2>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veritatis aperiam ipsam repellendus eos sint incidunt voluptatibus sequi nisi, a laboriosam, sit ratione officia consequatur corporis tenetur corrupti delectus aliquid.
+                    </p>
+
+                </div>
 
             </div>
 
--- src/components/main_component/PortfolioSection.jsx
+++ src/components/main_component/PortfolioSection.jsx
@@ -1,12 +1,50 @@
 import { useState } from "react";
-import AccordionItem from "./Accordion/Accordion";
+import ProjectCard from "./ProjectCard";
 // importiamo lo stile
 import style from "../Main.module.css";
 
+const portfolioData = [
+    {
+        id: 1,
+        immagine: "/public/monoscocca_carbonio.png",
+        tile: "Monoscocca",
+        titolo: "Progettazione Monoscocca in Carbonio",
+        descrizione: "Studio e realizzazione CAD/CAE di una monoscocca in fibra di carbonio per veicolo da competizione, con analisi FEM per verificare la resistenza strutturale."
+    },
+    {
+        id: 2,
+        immagine: "/public/simulazione_flussi.png",
+        tile: "Aerodinamica",
+        titolo: "CFD per Ottimizzazione Aerodinamica",
+        descrizione: "Simulazione fluidodinamica di un prototipo racing con l'obiettivo di ridurre la resistenza aerodinamica e migliorare il carico. Analisi eseguita su superfici in carbonio."
+    },
+    {
+        id: 3,
+        immagine: "/public/lavorazione_carbonio.png",
+        tile: "Laminazione",
+        titolo: "Produzione Manuale in Laminazione",
+        descrizione: "Realizzazione manuale di componenti strutturali in fibra di carbonio con tecnica wet layup e post-curing in autoclave. Controllo qualità e rifinitura inclusi."
+    },
+    {
+        id: 4,
+        immagine: "/public/lavorazione_carbonio.png",
+        tile: "Laminazione",
+        titolo: "Produzione Manuale in Laminazione",
+        descrizione: "Realizzazione manuale di componenti strutturali in fibra di carbonio con tecnica wet layup e post-curing in autoclave. Controllo qualità e rifinitura inclusi."
+    },
+    {
+        id: 5,
+        immagine: "/public/lavorazione_carbonio.png",
+        tile: "Laminazione",
+        titolo: "Produzione Manuale in Laminazione",
+        descrizione: "Realizzazione manuale di componenti strutturali in fibra di carbonio con tecnica wet layup e post-curing in autoclave. Controllo qualità e rifinitura inclusi."
+    }
+
+]
 
 export default function PortfolioSection() {
 
-    const [currentlyActive, setCurrentlyActive] = useState(2)
+    const [currentlyActive, setCurrentlyActive] = useState(null)
 
     const handleAccordionClick = (newActiveAccordion) => {
         setCurrentlyActive(newActiveAccordion === currentlyActive ? null : newActiveAccordion)
@@ -25,36 +63,17 @@ export default function PortfolioSection() {
                 {/* project card box  */}
                 <div className={style.project_card_box}>
                     {/* project card */}
-                    <div className={style.project_card}>
-                        {/* box image */}
-                        <div className={style.project_card_image}>
-                            <img src="./public/team_work.png"></img>
-                        </div>
-                        <AccordionItem
-                            isActive={currentlyActive === 1}
-                            onClick={() => handleAccordionClick(1)}
-                        />
-                    </div>
-                    <div className={style.project_card}>
-                        {/* box image */}
-                        <div className={style.project_card_image}>
-                            <img src="./public/team_work.png"></img>
-                        </div>
-                        <AccordionItem
-                            isActive={currentlyActive === 2}
-                            onClick={() => handleAccordionClick(2)}
-                        />
-                    </div>
-                    <div className={style.project_card}>
-                        {/* box image */}
-                        <div className={style.project_card_image}>
-                            <img src="./public/team_work.png"></img>
-                        </div>
-                        <AccordionItem
-                            isActive={currentlyActive === 3}
-                            onClick={() => handleAccordionClick(3)}
-                        />
-                    </div>
+                    {portfolioData.map(item => {
+                        return (
+                            <ProjectCard
+                                key={item.id}
+                                item={item}
+                                currentlyActive={currentlyActive}
+                                handleAccordionClick={handleAccordionClick}
+                            />
+                        )
+                    })}
+
                 </div>
             </div>
         </section >
