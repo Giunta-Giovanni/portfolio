7603d8f3aa4c2dfa9c35b73609581975d61390e4
GiuntaGiovanni <jhonniest741@gmail.com>
2025-04-15 13:46:28 +0200

    [feat] implement dynamic ExpertiseSection with ExpertiseCard component and add new training images

--- src/components/main_component/ExpertiesSection.jsx
+++ src/components/main_component/ExpertiesSection.jsx
@@ -1,84 +1,33 @@
 import style from "../Main.module.css";
+import expertiesData from "../../data/ExpertiesData";
+import { generateFeatureCards } from "../../functions/generateFeatureCards";
+
+import ExpertiseCard from "./ExpertiseCard";
+
 export default function ExpertiesSection() {
+
     return (
         <section className={style.experties_section}>
             <div className="container">
                 {/* riga dei box experienze */}
                 <div className={`row flex-nowrap ${style.experties_row}`}>
-                    {/* colonna */}
-                    <div className="col">
-                        <div className={style.expertise_box}>
-                        </div>
-                        <h4 className={style.expertise_title}>PRIMO</h4>
-                    </div>
-                    {/* colonna */}
-                    <div className="col">
-                        <div className={style.expertise_box}>
-                        </div>
-                        <h4 className={style.expertise_title}>SECONDO</h4>
-                    </div>
-                    {/* colonna */}
-                    <div className="col">
-                        <div className={style.expertise_box}>
-                        </div>
-                        <h4 className={style.expertise_title}>TERZO</h4>
-                    </div>
+                    {/* Experise Card */}
+                    {generateFeatureCards(expertiesData)}
                 </div>
 
                 {/* riga con formazioni*/}
                 <div className={style.container_training}>
-                    <div className="row py-4">
-                        {/* colonna con cerchio */}
-                        <div className="col-3 col-md-2">
-                            <div className={style.circle}></div>
-                        </div>
-                        {/* colonna con formazione */}
-                        <div className="col-9 col-md-10">
-                            <div className={style.box_training}>
-                                <h6>Titolo Formazione</h6>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque consectetur quos molestiae assumenda error non beatae corporis illo saepe accusantium at vel inventore, ex harum impedit facilis qui explicabo nisi!</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row py-4">
-                        {/* colonna con cerchio */}
-                        <div className="col-3 col-md-2">
-                            <div className={style.circle}></div>
-                        </div>
-                        {/* colonna con formazione */}
-                        <div className="col-9 col-md-10">
-                            <div className={style.box_training}>
-                                <h6>Titolo Formazione</h6>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque consectetur quos molestiae assumenda error non beatae corporis illo saepe accusantium at vel inventore, ex harum impedit facilis qui explicabo nisi!</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row py-4">
-                        {/* colonna con cerchio */}
-                        <div className="col-3 col-md-2">
-                            <div className={style.circle}></div>
-                        </div>
-                        {/* colonna con formazione */}
-                        <div className="col-9 col-md-10">
-                            <div className={style.box_training}>
-                                <h6>Titolo Formazione</h6>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque consectetur quos molestiae assumenda error non beatae corporis illo saepe accusantium at vel inventore, ex harum impedit facilis qui explicabo nisi!</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row py-4">
-                        {/* colonna con cerchio */}
-                        <div className="col-3 col-md-2">
-                            <div className={style.circle}></div>
-                        </div>
-                        {/* colonna con formazione */}
-                        <div className="col-9 col-md-10">
-                            <div className={style.box_training}>
-                                <h6>Titolo Formazione</h6>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque consectetur quos molestiae assumenda error non beatae corporis illo saepe accusantium at vel inventore, ex harum impedit facilis qui explicabo nisi!</p>
-                            </div>
-                        </div>
-                    </div>
+                    {expertiesData.map(item => {
+                        return (
+                            <ExpertiseCard
+                                key={item.id}
+                                item={item}
+                            />
+                        )
+                    })}
+
+
+
                 </div>
             </div>
         </section >
---
+++ src/components/main_component/ExpertiseCard.jsx
@@ -0,0 +1,19 @@
+import style from "../Main.module.css";
+
+export default function ExpertiseCard({ item }) {
+    return (
+        <div className="row py-4">
+            {/* colonna con cerchio */}
+            <div className="col-3 col-md-2 p-0 ">
+                <div className={style.circle}></div>
+            </div>
+            {/* colonna con formazione */}
+            <div className="col-9 col-md-10">
+                <div className={style.box_training}>
+                    <h6>{item.title}</h6>
+                    <p>{item.abstract}</p>
+                </div>
+            </div>
+        </div>
+    )
+}                   
