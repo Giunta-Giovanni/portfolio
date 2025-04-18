fe0aff61a91797da22c1549c0615e617f2e9997f
GiuntaGiovanni <jhonniest741@gmail.com>
2025-04-18 10:42:48 +0200

    refactor: improve comment clarity and consistency in ExpertiesSection.jsx

--- src/components/main_component/ExpertiesSection.jsx
+++ src/components/main_component/ExpertiesSection.jsx
@@ -1,22 +1,27 @@
+// import Style
 import style from "../Main.module.css";
+// import Data
 import expertiesData from "../../data/ExpertiesData";
+// import Functions
 import { generateFeatureCards } from "../../functions/generateFeatureCards";
-
+// import Components
 import ExpertiseCard from "./ExpertiseCard";
 
 export default function ExpertiesSection() {
 
     return (
         <section className={style.experties_section}>
+            {/* external container */}
             <div className="container">
-                {/* riga dei box experienze */}
+                {/* row box */}
                 <div className={`row flex-nowrap ${style.experties_row}`}>
-                    {/* Experise Card */}
+                    {/* feature card */}
                     {generateFeatureCards(expertiesData)}
                 </div>
 
-                {/* riga con formazioni*/}
+                {/* expertise box*/}
                 <div className={style.container_training}>
+                    {/* generate expertise card */}
                     {expertiesData.map(item => {
                         return (
                             <ExpertiseCard
@@ -25,9 +30,6 @@ export default function ExpertiesSection() {
                             />
                         )
                     })}
-
-
-
                 </div>
             </div>
         </section >
