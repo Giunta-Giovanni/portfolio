02324056fba6c7615451347d3b51111f84030cc4
GiuntaGiovanni <jhonniest741@gmail.com>
2025-08-21 15:31:13 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  //name of repository where our code is going to exist
+  base:"/deploy-test",
 })
